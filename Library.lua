fb10120d4e8e9e8849d41588daab5c14c4fe939f
Arcadian420 <kaclhhdo@gmail.com>
2023-08-23 23:18:16 +0200

    updated UI

--- Library.lua
+++ Library.lua
@@ -20,8 +20,9 @@ Quantum.Parent = game.CoreGui
 Quantum.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Frame.Parent = Quantum
-Frame.BackgroundColor3 = Color3.new(0.0196078, 0.0196078, 0.0196078)
-Frame.BorderColor3 = Color3.new(0.141176, 0.180392, 0.27451)
+Frame.BackgroundColor3 = Color3.new(0.027451, 0.027451, 0.0235294)
+Frame.BorderColor3 = Color3.new(0.101961, 0.113725, 0.145098)
+Frame.BorderSizePixel = 2
 Frame.Position = UDim2.new(0.435081691, 0, 0.345070422, 0)
 Frame.Size = UDim2.new(0, 480, 0, 339)
 
@@ -29,8 +30,8 @@ MakeDraggable(Frame)
 
 TopBar.Name = "TopBar"
 TopBar.Parent = Frame
-TopBar.BackgroundColor3 = Color3.new(0.0313726, 0.0313726, 0.0313726)
-TopBar.BorderColor3 = Color3.new(0.145098, 0.145098, 0.172549)
+TopBar.BackgroundColor3 = Color3.new(0.0352941, 0.0352941, 0.0352941)
+TopBar.BorderColor3 = Color3.new(0.0941176, 0.0941176, 0.0941176)
 TopBar.Position = UDim2.new(0.0148103079, 0, 0.0227917694, 0)
 TopBar.Size = UDim2.new(0, 466, 0, 26)
 
@@ -43,14 +44,14 @@ TextLabel.Position = UDim2.new(0.0188112706, 0, 0, 0)
 TextLabel.Size = UDim2.new(0, 123, 0, 26)
 TextLabel.Font = Enum.Font.Arial
 TextLabel.Text = text
-TextLabel.TextColor3 = Color3.new(0.447059, 0.447059, 0.776471)
+TextLabel.TextColor3 = Color3.new(0.443137, 0.431373, 0.678431)
 TextLabel.TextSize = 14
 TextLabel.TextStrokeTransparency = 0
 TextLabel.TextXAlignment = Enum.TextXAlignment.Left
 
 UnderLine.Name = "UnderLine"
 UnderLine.Parent = TopBar
-UnderLine.BackgroundColor3 = Color3.new(0.447059, 0.447059, 0.776471)
+UnderLine.BackgroundColor3 = Color3.new(0.443137, 0.431373, 0.678431)
 UnderLine.BorderColor3 = Color3.new(0, 0, 0)
 UnderLine.BorderSizePixel = 0
 UnderLine.Position = UDim2.new(0, 0, 1, 0)
@@ -61,8 +62,8 @@ CreateUnderLine(UnderLine)
 Tabs.Name = "Tabs"
 Tabs.Parent = Frame
 Tabs.Active = true
-Tabs.BackgroundColor3 = Color3.new(0.0313726, 0.0313726, 0.0313726)
-Tabs.BorderColor3 = Color3.new(0.145098, 0.145098, 0.172549)
+Tabs.BackgroundColor3 = Color3.new(0.0352941, 0.0352941, 0.0352941)
+Tabs.BorderColor3 = Color3.new(0.0941176, 0.0941176, 0.0941176)
 Tabs.BorderSizePixel = 0
 Tabs.Position = UDim2.new(0.0149999997, 0, 0.123000003, 0)
 Tabs.Size = UDim2.new(0, 147, 0, 291)
@@ -79,8 +80,8 @@ UIListLayout.Padding = UDim.new(0, 4)
 
 Container.Name = "Container"
 Container.Parent = Frame
-Container.BackgroundColor3 = Color3.new(0.0313726, 0.0313726, 0.0313726)
-Container.BorderColor3 = Color3.new(0.145098, 0.145098, 0.172549)
+Container.BackgroundColor3 = Color3.new(0.0352941, 0.0352941, 0.0352941)
+Container.BorderColor3 = Color3.new(0.0941176, 0.0941176, 0.0941176)
 Container.Position = UDim2.new(0.335416675, 0, 0.122999974, 0)
 Container.Size = UDim2.new(0, 312, 0, 290)
 
@@ -99,8 +100,7 @@ local UIPadding = Instance.new("UIPadding")
 
 TabButton.Name = text.."Button"
 TabButton.Parent = Tabs
-TabButton.BackgroundColor3 = Color3.new(1, 1, 1)
-TabButton.BackgroundTransparency = 1
+TabButton.BackgroundColor3 = Color3.new(0.380392, 0.380392, 0.388235)
 TabButton.BorderColor3 = Color3.new(0, 0, 0)
 TabButton.BorderSizePixel = 0
 TabButton.Size = UDim2.new(0, 147, 0, 20)
@@ -113,8 +113,10 @@ Frame_2.Parent = TabButton
 Frame_2.BackgroundColor3 = Color3.new(0.380392, 0.380392, 0.388235)
 Frame_2.BorderColor3 = Color3.new(0, 0, 0)
 Frame_2.BorderSizePixel = 0
-Frame_2.Position = UDim2.new(0.0340131894, 0, 0.0500000007, 0)
-Frame_2.Size = UDim2.new(0, 3, 0, 18)
+Frame_2.Position = UDim2.new(0.0340000018, 0, 0.100000001, 0)
+Frame_2.Size = UDim2.new(0, 3, 0, 17)
+
+CreateUIGradient(Frame_2)
 
 TextLabel_2.Parent = TabButton
 TextLabel_2.BackgroundColor3 = Color3.new(1, 1, 1)
@@ -158,8 +160,8 @@ v.Visible = false
         v.Frame.BackgroundColor3 = Color3.fromRGB(97, 97, 99)
         end
 end
-    TextLabel_2.TextColor3 = Color3.fromRGB(114, 114, 198)
-    Frame_2.BackgroundColor3 = Color3.fromRGB(114, 114, 198)
+    TextLabel_2.TextColor3 = Color3.fromRGB(113, 110, 173)
+    Frame_2.BackgroundColor3 = Color3.fromRGB(113, 110, 173)
     Container2.Visible = true
 end)
 end
